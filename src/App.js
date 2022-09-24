9feda7ae8ce52d92fb300e542e3ab9da987ba769
jitendra kumar <jitendra.kumar@techaheadcorp.com>
2022-09-24 17:18:30 +0530

    Enable Dark Mode with of useState

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
       {/* <Navbar title="{3}" aboutText="About Us"/> */}
       <Navbar />
-      <div className='container'>
+      {/* <div className='container'>
         <TextForm title='Enter the text' />
-      </div>
-      
+      </div> */}
+      <About title='About Page'/>
+
 
     </>
   );
