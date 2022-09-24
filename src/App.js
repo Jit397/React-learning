41c5f3c5ce329f6d00ade5ef30ea5b17095b90f2
jitendra kumar <jitendra.kumar@techaheadcorp.com>
2022-09-24 19:26:16 +0530

    dark conditionly

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
-
+import React, { useState } from 'react'
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+    }
+    else {
+      setMode('light');
+    }
+
+  }
   return (
     <>
-      {/* <Navbar title="{3}" aboutText="About Us"/> */}
-      <Navbar />
+      <Navbar mode={mode} title="Hello" toggleMode={toggleMode} aboutText="About Us" />
+      {/* <Navbar /> */}
       {/* <div className='container'>
         <TextForm title='Enter the text' />
       </div> */}
-      <About title='About Page'/>
+      <About title='About Page' />
 
 
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 function Navbar(props) {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-light">
+            <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="collapse navbar-collapse"  id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="/">Home</a>
@@ -25,6 +25,10 @@ function Navbar(props) {
                         </form>
                     </div>
                 </div>
+               
+                <div className={`form-check form-switch text-${props.mode === 'light'?'dark':'light'}`}>
+                    <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>                      
+                </div>
             </nav>
         </>
     )
@@ -33,10 +37,10 @@ function Navbar(props) {
 export default Navbar
 
 Navbar.propTypes = {
-    title:PropTypes.string.isRequired,
-    aboutText:PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    aboutText: PropTypes.string.isRequired
 };
 Navbar.defaultProps = {
-    title:'Hello jitendra',
-    aboutText:'ravi'
+    title: 'Hello jitendra',
+    aboutText: 'ravi'
 };
