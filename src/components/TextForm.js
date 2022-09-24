04d20666a8991ea4d019d69c8c04b4530b330fb0
jitendra kumar <jitendra.kumar@techaheadcorp.com>
2022-09-24 15:01:32 +0530

    uppercase and lowercase convert and counter or preview

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,6 +8,11 @@ function TextForm(props) {
         setText(newText);
     }
 
+    const handleLowerClick = () => {
+        console.log('Lower clicked');
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const onChangeHandler = (e) => {
         console.log('OnChange');
         setText(e.target.value);
@@ -20,10 +25,13 @@ function TextForm(props) {
                 <label htmlFor="exampleFormControlInput1" className="form-label">{props.title}</label>
                 <textarea className="form-control" onChange={onChangeHandler} id="myText" value={text} row='15'></textarea>
             </div>
-            <button className='btn btn-primary' onClick={handleUpClick}>Convert To Uppercase</button>
+            <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert To Uppercase</button>
+            <button className='btn btn-primary' onClick={handleLowerClick}>Convert To Lowercase</button>
             <div className='container my-4'>
                 <h1>Word counter</h1>
                 <p>Count {text.split(' ').length} words and lenth {text.length}</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
             </div>
         </>
     )
